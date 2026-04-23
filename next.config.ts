e1f974059135015bc01546cd1e0442d8ac3b88ab
praecantat0r <ashianashi@gmail.com>
2026-04-23 10:37:33 +0200

    chromium

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/render': ['./node_modules/@sparticuz/chromium/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/api/render': ['./node_modules/@sparticuz/chromium/**/*'],
   },
   images: {
     remotePatterns: [
